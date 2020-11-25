4cc1dbf452ccd494097216e4ab41efae16160d7e
rsyed0 <reeditsyed@gmail.com>
2020-11-25 14:18:48 -0800

    added node_modules

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testServer");
 var app = express();
 
 // view engine setup
@@ -21,7 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/testServer", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
