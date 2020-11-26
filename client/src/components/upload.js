73d654718b97fd394a5779b8b8e27cb8675df8eb
rsyed0 <reeditsyed@gmail.com>
2020-11-26 15:24:23 -0800

    fix client

--- client/src/components/upload.js
+++ client/src/components/upload.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from "react"
  
-const Upload = () => {
-	return (
-		<div>
-			<h1>Upload</h1>
-			<p>Upload page body content</p>
-		</div>
-	);
+class Upload extends Component {
+	render(){
+		return (
+			<div>
+				<h1>Upload</h1>
+				<p>Upload page body content</p>
+			</div>
+    	);
+	}
 }
  
 export default Upload;
