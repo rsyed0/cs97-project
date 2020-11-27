6134c574a7ceebac7eda0f81308ede3a55b9cdf8
Forrest Burton <burton.forrest10@gmail.com>
2020-11-26 18:46:03 -0800

    Connected app to firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 /* FROM https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/ */
 /* TODO add multi page application functionality from https://www.golangprograms.com/how-to-create-simple-react-router-to-navigate-multiple-pages.html */
 import React, { Component } from "react"
-import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
