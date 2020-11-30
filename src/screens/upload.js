6064e3cb185c1a0bd5bdd7368b305dbb59e4ae82
Forrest Burton <burton.forrest10@gmail.com>
2020-11-29 18:53:24 -0800

    Added title and 2 text fields

--- src/screens/upload.js
+++ src/screens/upload.js
@@ -2,6 +2,7 @@ import React, {useContext} from "react"
 
 import firebase from "../firebase";
 import { AuthContext } from "../firebaseauth";
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
 
 import { useHistory } from "react-router-dom";
 import { withRouter } from "react-router";
@@ -10,6 +11,8 @@ import { withRouter } from "react-router";
 // class for uploading a video
 const UploadScreen = () => {
 	const history = useHistory();
+	const { Title } = Typography;
+	const  { TextArea } = Input;
 
 	function gotoHome(){
 		history.push("/home");
@@ -26,15 +29,76 @@ const UploadScreen = () => {
 
 	function gotoLanding (){
 		history.push("/");
-	}
+    }
+    
+    const onSubmit = () => {
+        
+    }
 
 
 	return (
-		<div>
-			<h1>Upload Video</h1>
-			<button onClick={uploadVideoFile}>Upload</button>
-			<button onClick={gotoLanding}>Back</button>
-		</div>
+		<div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+        <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+            <Title level={2} > Upload Video</Title>
+        </div>
+
+        <Form onSubmit={onSubmit}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                {/* <Dropzone 
+                    multiple={false}
+                    maxSize={800000000}>
+                    {({ getRootProps, getInputProps }) => (
+                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                            {...getRootProps()}
+                        >
+                            <input {...getInputProps()} />
+                            <Icon type="plus" style={{ fontSize: '3rem' }} />
+
+                        </div>
+                    )}
+                </Dropzone> */}
+
+                {/* {thumbnail !== "" &&
+                    <div>
+                        <img src={`http://localhost:5000/${thumbnail}`} alt="haha" />
+                    </div>
+                } */}
+            </div>
+
+            <br /><br />
+            <label>Title</label>
+            <Input
+                //  onChange={handleChangeTitle}
+                //  value={title}
+            />
+            <br /><br />
+            <label>Description</label>
+            <TextArea
+                //  onChange={handleChangeDecsription}
+                //  value={Description}
+            />
+            <br /><br />
+
+            {/* <select onChange={handleChangeOne}>
+                {Private.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label}</option>
+                ))}
+            </select> */}
+            <br /><br />
+
+            {/* <select onChange={handleChangeTwo}>
+                {Catogory.map((item, index) => (
+                    <option key={index} value={item.label}>{item.label}</option>
+                ))}
+            </select> */}
+            <br /><br />
+
+            {/* <Button type="primary" size="large" onClick={onSubmit}>
+                Submit
+            </Button> */}
+
+        </Form>
+    </div>
 	);
 }
 
