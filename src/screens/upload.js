e1ceff0c1bc7764ef161d7abfa9e012f620a77a6
Forrest Burton <burton.forrest10@gmail.com>
2020-12-01 16:31:21 -0800

    Linked upload.js to class and added geolocation

--- src/screens/upload.js
+++ src/screens/upload.js
@@ -1,140 +1,145 @@
-import React, {useContext, useState, useEffect} from "react"
+import React, { Component } from "react"
 
 import firebase from "../firebase";
 import { AuthContext } from "../firebaseauth";
 import { Typography, Button, Form, message, Input } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import Dropzone from 'react-dropzone'
+import { usePosition } from 'use-position';
 
-import { useHistory } from "react-router-dom";
 import { withRouter } from "react-router";
 
 // TODO setup firebase storage, link with this
 // class for uploading a video
 
-const UploadScreen = () => {
-    
-    const history = useHistory();
-	const { Title } = Typography;
-    const  { TextArea } = Input;
+class UploadScreen extends React.Component {
 
-    const Catogory = [
-        { value: 0, label: "Skating" },
-        { value: 0, label: "Basketball" },
-        { value: 0, label: "Nature" },
-        { value: 0, label: "Soccer" },
-        { value: 0, label: "Football" },
-        { value: 0, label: "Hockey" },
-        { value: 0, label: "Baseball" },
-    ]
+    constructor (props){
+        super(props);
 
-    const [Description, setDescription] = useState("");
-    const [Categories, setCategories] = useState("")
+        this.state = {
+            userLat: null,
+            userLng: null,
+            videoId: null,
+            Description: "",
+            Categories: "", 
+            Catogory: [
+                { value: 0, label: "Skating" },
+                { value: 0, label: "Basketball" },
+                { value: 0, label: "Nature" },
+                { value: 0, label: "Soccer" },
+                { value: 0, label: "Football" },
+                { value: 0, label: "Hockey" },
+                { value: 0, label: "Baseball" },
+            ],
+            Title: Typography,
+            TextArea: Input
+        };
+        this.onSubmit = this.onSubmit.bind(this)
+        this.onDrop = this.onDrop.bind(this)
+    }
 
-    const handleChangeDecsription = (event) => {
+
+    handleChangeDecsription = (event) => {
         console.log(event.currentTarget.value)
 
-        setDescription(event.currentTarget.value)
+        this.setState({
+            Description: event.currentTarget.value
+        });
     }
 
-    const handleChangeTwo = (event) => {
-        setCategories(event.currentTarget.value)
+    handleChangeTwo = (event) => {
+        this.setState({
+            Categories: event.currentTarget.label
+        });
     }
 
-	function gotoHome(){
-		history.push("/home");
-	}
 
-	const { currentUser } = useContext(AuthContext);
-
-	function uploadVideoFile(){
-		// check if file matches accepted formats
-		// auto-ID video, store with Firebase Storage
-		// log video in posts and under user in Firestore
+    uploadVideoFile(){
+        // check if file matches accepted formats
+        // auto-ID video, store with Firebase Storage
+        // log video in posts and under user in Firestore
         // use FieldValue.increment(1) to update numVideos
     }
 
     //generates random firestore id
-    const firestoreAutoId = () => {
+    firestoreAutoId = () => {
         const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-      
+    
         let autoId = ''
-      
+    
         for (let i = 0; i < 20; i++) {
-          autoId += CHARS.charAt(
+        autoId += CHARS.charAt(
             Math.floor(Math.random() * CHARS.length)
-          )
+        )
         }
         return autoId
-      }
+    }
     
     //TO DO: implement check that file is a video file and matches accepted formats
     //figure out where to store video in a place where we can later retrieve it 
-    const onDrop = ( files ) => {
+    onDrop = ( files ) => {
         
         const file = files[0];
         
     }
-
-	function gotoLanding (){
-		history.push("/");
-    }
     
 
-    const onSubmit = () => {
-        var userLat = null, userLong = null;
+    onSubmit = () => {
 
         //get reference to posts collection in firestore and generate random postID
         const post_ref = firebase.firestore().collection("posts");
-        const randomID = firestoreAutoId();
+        
+        if (this.state.postID == null) {
+            const randomID = this.firestoreAutoId();
+            this.setState({
+                postID: randomID
+            });
+        }
 
-		// use FirebaseUser id, not random/auto
+        // use FirebaseUser id, not random/auto
         const currentUser = firebase.auth().currentUser;
-        const UID = currentUser.uid;
 
-		if (!currentUser){
-			return false;
+        if (!currentUser){
+            alert("Please log in")
+            return false;
         } 
         else {
-            const time_stamp = Date.now(); // unformatted time stamp
-            
             //gives formatted time
             //alert(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(time_stamp));
 
-            if (navigator.geolocation) {
-                alert("F")
-                navigator.geolocation.watchPosition(function(position) {
-                  alert("hey");
-                  alert(position.coords.latitude);
-                  alert(position.coords.longitude);
-                  userLat = position.coords.latitude;
-                  userLong = position.coords.longitude;
+            navigator.geolocation.getCurrentPosition( (position) => {
+                this.setState({
+                  userLat: position.coords.latitude,
+                  userLng: position.coords.longitude,
                 });
-            }
-
-            var postData = {
-                postID: randomID,
-                userId: UID,
-                timestamp: time_stamp,
-                lat: userLat,
-                lng: userLong,
-                likes: 0,
-                desciption: Description,
-                sport: Categories
-            }
-
-            post_ref.add(postData)
-            .then(function() {
-                alert("Document uploaded succesfully");
-            })
-            .catch(function() {
-               alert("ERROR");
+               
+                const time_stamp = Date.now(); // unformatted time stamp
+                const UID = currentUser.uid; //get userID
+                
+                var postData = {
+                    postID: this.state.postID,
+                    userId: UID,
+                    timestamp: time_stamp,
+                    lat: this.state.userLat,
+                    lng: this.state.userLng,
+                    likes: 0,
+                    desciption: this.state.Description,
+                    sport: this.state.Categories
+                }
+                post_ref.add(postData)
+                .then(function() {
+                    alert("Document uploaded succesfully");
+                })
+                .catch(function() {
+                    alert("ERROR");
+                })
             });
             
             //submit button, has not been clicked yet. Video hasn't officially "posted". So we set this to false
             //!!!BELOW STILL NEEDS TO BE TESTED. message to fix signup.js
             // const user_doc = firebase.firestore().collection("users").doc(currentUser.uid);
-			// user_doc.collection("videos").doc(randomID).update({
+            // user_doc.collection("videos").doc(randomID).update({
             //     postId: randomID,
             //     posted: false
             // });
@@ -146,86 +151,90 @@ const UploadScreen = () => {
             // })
 
             // user_doc.set({
-			// 	numVideos: vidNum
-			// });
-		}
+            // 	numVideos: vidNum
+            // });
+        }
         // const user_ref = firebase.firestore().collection("users");
         // const post_ref = firebase.firestore().collection("posts");
 
-		// // use FirebaseUser id, not random/auto
-		// const currentUser = firebase.auth().currentUser;
-		// if (!currentUser){
-		// 	return false;
-		// } else {
-		// 	user_ref.doc(currentUser.uid).set({
-		// 		email: email,
-		// 		userId: currentUser.uid,
-		// 		numFollowers: 0,
-		// 		numFollowing: 0,
-		// 		numVideos: 0
-		// 	});
-		// 	ref.doc(currentUser.uid).collection("followers").add("");
-		// 	ref.doc(currentUser.uid).collection("following").add("");
-		// 	ref.doc(currentUser.uid).collection("videos").add("");
-		// }
+        // // use FirebaseUser id, not random/auto
+        // const currentUser = firebase.auth().currentUser;
+        // if (!currentUser){
+        // 	return false;
+        // } else {
+        // 	user_ref.doc(currentUser.uid).set({
+        // 		email: email,
+        // 		userId: currentUser.uid,
+        // 		numFollowers: 0,
+        // 		numFollowing: 0,
+        // 		numVideos: 0
+        // 	});
+        // 	ref.doc(currentUser.uid).collection("followers").add("");
+        // 	ref.doc(currentUser.uid).collection("following").add("");
+        // 	ref.doc(currentUser.uid).collection("videos").add("");
+        // }
+    }
+        
+    
+
+    render() {
+        return (
+            <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <this.state.Title level={2} > Upload Video</this.state.Title>
+            </div>
+    
+            <Form onSubmit={this.onSubmit}>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Dropzone 
+                        multiple={false}
+                        maxSize={800000000}
+                        onDrop={this.onDrop}>
+                        {({ getRootProps, getInputProps }) => (
+                            <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                {...getRootProps()}
+                            >
+                                <input {...getInputProps()} />
+                                <UploadOutlined style={{ fontSize: '3rem' }} />
+    
+                            </div>
+                        )}
+                    </Dropzone> 
+    
+                    {/* {thumbnail !== "" &&
+                        <div>
+                            <img src={`http://localhost:5000/${thumbnail}`} alt="haha" />
+                        </div>
+                */} 
+                </div>
+    
+    
+                <div style={{ margin: '24px 0' }} />
+                <label>Description</label>
+                <br></br>
+                <this.state.TextArea placeholder="video description" showCount maxLength={100}
+                    onChange={this.handleChangeDecsription}
+                    value={this.state.Description}
+                />
+            <div style={{ margin: '24px 0' }} />
+    
+                <label>Sport</label>
+                <br></br>
+                <select onChange={this.handleChangeTwo} value={this.state.Categories}>
+                    {this.state.Catogory.map((item, index) => (
+                        <option key={index} value={item.label}>{item.label}</option>
+                    ))}
+                </select>
+                <div style={{ margin: '40px 0' }} />
+    
+                <Button type="primary" size="large" onClick={this.onSubmit}>
+                    Submit
+                </Button>
+    
+            </Form>
+        </div>
+        );
     }
 
-
-	return (
-		<div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-        <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
-            <Title level={2} > Upload Video</Title>
-        </div>
-
-        <Form onSubmit={onSubmit}>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Dropzone 
-                    multiple={false}
-                    maxSize={800000000}
-                    onDrop={onDrop}>
-                    {({ getRootProps, getInputProps }) => (
-                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-                            {...getRootProps()}
-                        >
-                            <input {...getInputProps()} />
-                            <UploadOutlined style={{ fontSize: '3rem' }} />
-
-                        </div>
-                    )}
-                </Dropzone> 
-
-                {/* {thumbnail !== "" &&
-                    <div>
-                        <img src={`http://localhost:5000/${thumbnail}`} alt="haha" />
-                    </div>
-              */} 
-            </div>
-
-
-            <div style={{ margin: '24px 0' }} />
-            <label>Description</label>
-            <TextArea placeholder="video description" showCount maxLength={100}
-                onChange={handleChangeDecsription}
-                value={Description}
-            />
-           <div style={{ margin: '24px 0' }} />
-
-            <label>Sport</label>
-            <br></br>
-            <select onChange={handleChangeTwo} value={Categories}>
-                {Catogory.map((item, index) => (
-                    <option key={index} value={item.label}>{item.label}</option>
-                ))}
-            </select>
-            <div style={{ margin: '40px 0' }} />
-
-            <Button type="primary" size="large" onClick={onSubmit}>
-                Submit
-            </Button>
-
-        </Form>
-    </div>
-	);
 }
-
 export default withRouter(UploadScreen);
