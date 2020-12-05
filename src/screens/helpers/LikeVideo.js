a236d93473b3173e6a236b39cede92553f58f965
Forrest Burton <burton.forrest10@gmail.com>
2020-12-04 18:20:30 -0800

    Implemented like and unlike video

--- src/screens/helpers/LikeVideo.js
+++ src/screens/helpers/LikeVideo.js
@@ -1,37 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { Tooltip } from 'antd';
 import { LikeOutlined } from '@ant-design/icons';
+import firebase from "../../firebase";
+
+let currently_liked = false;
 
 function LikeVideo(props) {
 
     const [Likes, setLikes] = useState(0)
     const [LikeAction, setLikeAction] = useState(null)
+
     let variable = {};
 
     if (props.video) {
         variable = { videoId: props.videoId, userId: props.userId }
-    } else {
-        variable = { commentId: props.commentId, userId: props.userId }
+    // } else {
+    //     variable = { commentId: props.commentId, userId: props.userId }
     }
-
     
 
-
     useEffect(() => {
+        //get video metedata from firestore to get number of video likes
+        const post_ref = firebase.firestore().collection("posts");
+        post_ref.doc(props.videoId).get().then(function(doc) {
+            //get number of likes on video
+            setLikes(doc.data().likes)
 
-        
+            //if I already click this like button or not 
+            if (currently_liked === false) {
+                setLikeAction('liked')
+            }
+        });
+    }, [props.videoId])
 
-    }, [])
 
     const onLike = () => {
-
-        if (LikeAction === null) {
-
-
-        } else {
-
+        //increase likes by 1
+        const post_ref = firebase.firestore().collection("posts");
+        if (currently_liked === false) {
+            post_ref.doc(props.videoId).update({
+               likes: (Likes + 1)
+            });
+            currently_liked = true;
+            setLikes(Likes+1);
+        } else {   //unlike, decrease likes by 1
+            post_ref.doc(props.videoId).update({
+                likes: (Likes - 1)
+            });
+            currently_liked = false;
+            setLikes(Likes-1);
         }
-
     }
 
 
